3e29e64eec54b54948d613d9e351db39b64f0dac
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-18 14:02:44 -0800

    created form compenent, BlogForm.jsx file and added provider component to index.js

---
+++ src/components/form/BlogForm.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { createBlog } from '../../actions/blogActions';
+import { useDispatch } from '../../state/BlogProvider';
+
+const BlogForm = () => {
+    const dispatch = useDispatch();
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState(1);
+    const [body, setBody] = useState('');
+
+    const handleSubmit = event => {
+        event.PreventDefault();
+
+        dispatch(createBlog({ title, author, body}));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={({ target }) => setName(target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Author"
+                value={author}
+                onChange={({ target }) => setAuthor(target.value)}
+            />
+            <input
+                text="text"
+                placeholder="Write something here"
+                value={body}
+                onChange={({ target }) => setBody(target.value)}
+            />
+        </form>          
+    );
+};
+
+export default BlogForm;
