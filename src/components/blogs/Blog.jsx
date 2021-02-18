32eb56ad4026cc98e87d20306228a118a08fd2fb
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-18 14:26:34 -0800

    added Blog.jsx file

---
+++ src/components/blogs/Blog.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const Blog = () => {
+    return(
+        <dl>
+            <dt>Title</dt>
+            <dd>{title}</dd>
+
+            <dt>Author</dt>
+            <dd>{author}</dd>
+
+            <dt>Body</dt>
+            <dd>{body}</dd>
+        </dl>
+
+    );
+};
+
+
+Blog.propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+
+};
