b4f8f95946f73a7b73bf4104c1cf1b65a9ff5a73
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-18 22:35:55 -0800

    created a test for deleteComment via user and is passing

--- src/actions/commentActions.test.js
+++ src/actions/commentActions.test.js
@@ -1,4 +1,4 @@
-import { createComment, CREATE_COMMENT, DELETE_COMMENT } from './commentActions';
+import { createComment, CREATE_COMMENT, deleteComment, DELETE_COMMENT } from './commentActions';
 
 describe('commentAction for creating a comment', () => {
     it('creates a comment', () => {
@@ -10,4 +10,16 @@ describe('commentAction for creating a comment', () => {
         payload: { comment: 'cool post' } 
     });
 });
+
+    it('deletes a comment via the user', () => {
+    const action = deleteComment({
+      user: '@pinay_noire'
+    });
+
+    expect(action).toEqual({
+      type: DELETE_COMMENT,
+      payload: { user: '@pinay_noire' }
+    });
+  });
 });
+
